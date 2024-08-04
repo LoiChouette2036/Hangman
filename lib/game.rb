131b539b5c5b0b72bebe8666ff13daf73805525a
LoiChouette2036 <tristanbellanger10@gmail.com>
2024-08-04 09:03:17 +0900

    loading game

--- lib/game.rb
+++ lib/game.rb
@@ -2,23 +2,55 @@ require_relative 'secret_word'
 
 class Game
 
+    attr_reader :number_of_try, :word_to_find
+
     # initialize the game with the secret word
     def initialize(word_to_find)
         @word_to_find = word_to_find
         @array_to_discover = Array.new(@word_to_find.length,"_")
+        @number_of_try = 12
+        @array_with_incorrect_guess = Array.new
     end
 
 
     def display
         puts @array_to_discover.join(" ")
+        puts ""
     end
 
     def guess_user
-        puts "Choose one letter"
-        @letter_guess = gets.chomp
+        puts "Choose one letter or type 'save' to save the game"
+        @letter_guess = gets.chomp.downcase
+        if @letter_guess == 'save'
+            save_game
+            return nil
+        end
+        @letter_guess
+    end
+
+    def save_game
+        Dir.mkdir('saves') unless Dir.exist?('saves') # Create saves directory if it doesn't exist
+        File.open('saves/hangman_save.txt', 'w') do |file|
+            file.puts Marshal.dump(self) # Serialize the current game state
+        end
+        puts "Game saved!"
+    end
+
+    def self.load_game
+        if File.exist?('saves/hangman_save.txt')
+            saved_game = File.open('saves/hangman_save.txt', 'r') {|file| Marshal.load(file)}
+            puts "Game loaded!"
+            return saved_game
+        else
+            puts "No saved game found."
+            return nil
+        end
     end
 
     def inside_or_not (user_guess, word_being_guessed)
+        return if user_guess.nil? # Exit the method if user_guess is nil
+
+        user_guess.downcase!
         match_found = false
         word_being_guessed.each_with_index do |element, index|
             if element == user_guess
@@ -26,6 +58,24 @@ class Game
                 match_found = true
             end            
         end
-        puts "it's not inside" unless match_found
+        unless match_found
+            puts "it's not inside" 
+            @array_with_incorrect_guess.push(user_guess)
+            #puts "array of used letter #{@array_with_incorrect_guess}"
+        end
+        display
+        puts "array of used letter #{@array_with_incorrect_guess}"
+        @number_of_try -= 1;
     end
+
+    def win?
+        if @array_to_discover == @word_to_find.chars
+            #puts "you have won"
+            return true
+        else
+            puts "rounds left : #{@number_of_try}"
+            return false
+        end
+    end
+    
 end
--- main.rb
+++ main.rb
@@ -1,14 +1,50 @@
 require_relative 'lib/secret_word'
 require_relative 'lib/game'
 
-#Create an instance of SecretWord to get a word
-secret_word_instance = SecretWord.new
+# Option to load a saved game
+puts "Do you want to load a saved game (yes?no)"
+load_game = gets.chomp.downcase
 
-# Retrieve the secret word from SecretWord instance
-secret_word = secret_word_instance.word_to_find
+if load_game == 'yes'
+    game = Game.load_game
+    if game.nil?
+        puts "No saved game found. Starting a new game..."
+    end
+end
 
-# Pass the secret word to the Game instance
-game = Game.new(secret_word)
+# Create a new game if no saved game was loaded
+unless game
+    puts "Starting a new game..."
+    #Create an instance of SecretWord to get a word
+    secret_word_instance = SecretWord.new
 
+    # Retrieve the secret word from SecretWord instance
+    secret_word = secret_word_instance.word_to_find
+
+    # Pass the secret word to the Game instance
+    game = Game.new(secret_word)
+else
+    secret_word = game.word_to_find
+end
+
+
+puts "the game begin: "
 game.display
-game.guess_user
+
+#main game loop 
+while game.number_of_try > 0 
+    guessed_letter = game.guess_user
+    break if guessed_letter.nil? # Exit loop if game was saved
+    game.inside_or_not(guessed_letter, secret_word.chars)
+    game.display
+    break if game.win? # Break the loop if the player has won
+end
+
+if game.win?
+    puts "you won !!!"
+else
+    puts "you lose..."
+    puts "the word to find was #{secret_word}"
+end
+
+
